c5ddc0e91965c8d5e58c33cd8fc2b25ddc623ca2
yutongwang123 <yutong.wang123@gmail.com>
2024-06-15 19:20:54 +0800

    delete sth useless

--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -1,22 +1,4 @@
-﻿/**
-* This file is part of OA-SLAM.
-*
-* Copyright (C) 2022 Matthieu Zins <matthieu.zins@inria.fr>
-* (Inria, LORIA, Université de Lorraine)
-* OA-SLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* OA-SLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with OA-SLAM. If not, see <http://www.gnu.org/licenses/>.
-*/
-
+﻿
 
 /**
 * This file is part of ORB-SLAM2.
